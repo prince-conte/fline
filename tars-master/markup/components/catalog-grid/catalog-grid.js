7148c737493efbf706a52130f3d63ea20abc3758
Francesco <prince-conte@outlook.com>
2017-05-11 01:24:33 +0300

    slider(fix)

--- tars-master/markup/components/catalog-grid/catalog-grid.js
+++ tars-master/markup/components/catalog-grid/catalog-grid.js
@@ -1,29 +1,51 @@
 $(function(){
-
-
-  $('.js-catalog-grid-slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: false,
-    arrows: false,
-    fade: true,
-    autoplaySpeed: 500,
-    dots: false
-  });
   
-  var $this = $(this);
-
-$('.js-slider-hover').on('mouseover',function(){
+  var BREAKPOINT_MOBILE_INT = 767;
+  var BREAKPOINT_DESKTOP_INT = 768;
+  var BREAKPOINT_MOBILE  = '(max-width: ' + BREAKPOINT_MOBILE_INT + 'px)';
+  var BREAKPOINT_DESKTOP = '(min-width: ' + BREAKPOINT_DESKTOP_INT + 'px)';
   
-  $this = $(this).find('.js-catalog-grid-slider');
-  $this.slick('slickPlay');
   
-});
+  function initCatalogSlider() {
     
-$('.js-slider-hover').on('mouseout',function(){
-  $this.slick('slickPause');
+  if (Modernizr.mq(BREAKPOINT_DESKTOP)) {
   
+    $('.js-catalog-grid-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: false,
+      arrows: false,
+      fade: true,
+      autoplaySpeed: 500,
+      dots: false
+    });
+   } 
+    
+    var $this = $(this);
+
+    $('.js-slider-hover').on('mouseover',function(){
+
+      $this = $(this).find('.js-catalog-grid-slider');
+      $this.slick('slickPlay');
+
+    });
+
+    $('.js-slider-hover').on('mouseout',function(){
+      $this.slick('slickPause');
+
+    });
+  }
+
+  
+  $(window)
+      .load(function() {
+          initCatalogSlider();
+      })
+      .resize(function() {
+          initCatalogSlider();
+      });
+
+
 });
   
-  
-});
+
