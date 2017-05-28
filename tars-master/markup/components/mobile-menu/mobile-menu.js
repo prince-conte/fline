736a3bdb65fa044668b79d6b6a28c8968bff30dd
Francesco <prince-conte@outlook.com>
2017-05-28 19:10:18 +0300

    popup(full)

--- tars-master/markup/components/mobile-menu/mobile-menu.js
+++ tars-master/markup/components/mobile-menu/mobile-menu.js
@@ -4,14 +4,14 @@ $(function(){
     
     $('.js-mobile-fade').fadeIn(500);
     $('.js-mobile-menu').delay(500).fadeIn(500);
-    
+    return false
   });
     
   $('.js-mobile-menu-close').on('click', function () {
     
     $('.js-mobile-fade').delay(500).fadeOut(500);
     $('.js-mobile-menu').fadeOut(500);
-    
+    return false
   });
   
 });
---
+++ tars-master/markup/components/popup-form/popup-form.js
@@ -0,0 +1,7 @@
+
+  $('.js-mobile-menu-close').on('click', function () {
+    
+    $('.js-mobile-fade').delay(500).fadeOut(500);
+    $('.js-popup-form').fadeOut(500);
+    return false
+  });
